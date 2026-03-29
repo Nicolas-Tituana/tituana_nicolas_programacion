5306ae84a509d52bfb402fe8221be826e087970b
nicolas.tituana <nicolas.tituana@udla.edu.ec>
2026-03-29 17:07:52 -0500

    version final ejercico uno con los errores corregidos

--- tituana_nicolas_ejercicio1.c
+++ tituana_nicolas_ejercicio1.c
@@ -4,16 +4,21 @@ int main() {
     int inicio, fin, incremento;
 
     printf("Ingrese el numero inicial: ");
-    scanf("%d", inicio);
+    scanf("%d", &inicio);
 
     printf("Ingrese el numero final: ");
-    scanf("%d", &fin)
+    scanf("%d", &fin);
 
     printf("Ingrese el incremento: ");
     scanf("%d", &incremento);
+    
+    if (incremento <=0){
+    printf("El incremento debe ser positivo.\n");
+    return 1;
+    }
 
-    for (int i = inicio; i <= fin; i + incremento) {
-        printf("%d\n", i)
+    for (int i = inicio; i <= fin; i += incremento) {
+        printf("%d\n", i);
     }
 
     return 0;
